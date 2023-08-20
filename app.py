c64e4a2b407759568383c52ff4a6abbc1b212401
MaximilianoReyes <maximilianoreyesruiz@gmail.com>
2023-08-20 09:35:10 -0600

    test workflow

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ import aws_cdk as cdk
 from stacks.aplication_stack import ApplicationStack
 
 app = cdk.App()
-ApplicationStack(app, "ApplicationStack")
+ApplicationStack(app, "ApplicationStack") # AWS Mantiene un formato para lo nombres, nunca debe empezar con minusculas  
 
 app.synth()
--- stacks/aplication_stack.py
+++ stacks/aplication_stack.py
@@ -1,7 +1,12 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    aws_s3 as s3,
+    aws_s3 as s3
+    # aws_ecs_patterns as ecs_patterns,
+    # aws_ecs as ecs,
+    # aws_ec2 as ec2,
+    # aws_iam as iam,
+    # aws_ecr as ecr,
 )
 from constructs import Construct
 
@@ -11,11 +16,56 @@ class ApplicationStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
         
         bucket = s3.Bucket(self, "MyFirstBucket")
+        
+        # docker_tag = self.node.try_get_context("docker_tag")
+        # mongodb_uri = self.node.try_get_context("mongodb_uri")
 
-        # The code that defines your stack goes here
+        # vpc = ec2.Vpc(
+        #     self,
+        #     "EcsClusterVpc",
+        #     max_azs=2,
+        # )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "XampleQueue",
-        #     visibility_timeout=Duration.seconds(300),
+        # cluster = ecs.Cluster(
+        #     self,
+        #     "EcsCluster",
+        #     vpc=vpc,
+        # )
+
+        # ecs_task_role = iam.Role(
+        #     self,
+        #     "EcsTaskRole",
+        #     assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+        #     description="Grant access to multiple AWS services",
+        # )
+
+        # ecr_repository = ecr.Repository.from_repository_name(
+        #     self,
+        #     "EcrRepository",
+        #     "demo-fastapi-mongodb",
+        # )
+
+        # ecr_image = ecs.ContainerImage.from_ecr_repository(
+        #     ecr_repository,
+        #     docker_tag,
+        # )
+
+        # fargate_cluster = ecs_patterns.ApplicationLoadBalancedFargateService(
+        #     self,
+        #     "EcsFargateService",
+        #     cluster=cluster,
+        #     memory_limit_mib=1024,
+        #     cpu=512,
+        #     desired_count=1,
+        #     task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+        #         image=ecr_image,
+        #         task_role=ecs_task_role,
+        #         environment={
+        #             "MONGODB_URI": mongodb_uri
+        #         }
+        #     ),
+        # )
+
+        # fargate_cluster.target_group.configure_health_check(
+        #     path="/healthcheck"
         # )
